e065bf2bc8b5d760a14ead2f5619b6c0905519d8
Jackson Merma <mermaportocarreroj@gmail.com>
2023-10-08 01:10:38 -0500

    modelos agregados para animal y derivados

--- animales/admin.py
+++ animales/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Animal, InfoAnimal, NivelPeligro, Especie, Habitat, Coordenadas
 
-# Register your models here.
+admin.site.register(Animal)
+admin.site.register(InfoAnimal)
+admin.site.register(NivelPeligro)
+admin.site.register(Especie)
+admin.site.register(Habitat)
+admin.site.register(Coordenadas)
--- animales/models.py
+++ animales/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Animal(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #def __str__(self):
+    #   return self.nombre
+
+class InfoAnimal(models.Model):
+    animal = models.OneToOneField(Animal, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    nombreCientifico = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    ultimaModificacion = models.DateTimeField(auto_now=True)
+    calificacion = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(6)])
+
+class NivelPeligro(models.Model):
+    infoAnimal = models.OneToOneField(InfoAnimal, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    color = models.CharField(max_length=100)
+
+class Especie(models.Model):
+    infoAnimal = models.OneToOneField(InfoAnimal, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+
+class Habitat(models.Model):
+    infoAnimal = models.OneToOneField(InfoAnimal, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+
+class Coordenadas(models.Model):
+    infoAnimal = models.ForeignKey(InfoAnimal, on_delete=models.CASCADE)
+    coordenada = models.CharField(max_length=100)
